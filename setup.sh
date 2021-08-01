067ee2d594ce1f7960d7f46df2fc81f31691fe6b
aishu136 <46559921+aishu136@users.noreply.github.com>
2021-08-01 21:11:19 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"email@domain\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "
+[server]n
+headless = truen
+port = $PORTn
+enableCORS = falsen
+n
 " > ~/.streamlit/config.toml
